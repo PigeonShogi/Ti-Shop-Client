c7a20faaf2992488b95768583aa9999564c49b12
Ckai <ckai.trans@gmail.com>
2023-07-13 15:51:54 +0800

    setting up development environment

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SignInForm from "./components/SignInForm.vue";
+</script>
+
+<template>
+  <div>
+    <h1>Welcome!</h1>
+  </div>
+  <SignInForm msg="登入表單建置中" />
+</template>
+
+<style scoped></style>
