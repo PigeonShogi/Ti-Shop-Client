849e124f64f83b2b87a58478dcd099b19d1a54f2
Ckai <ckai.trans@gmail.com>
2023-07-13 19:31:19 +0800

    add bootstrap

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -1,23 +1,52 @@
 <script setup>
-import { ref } from "vue";
-
 defineProps({
   msg: String,
 });
-
-const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+  <div class="sign-in-form">
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" class="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.sign-in-form {
+  border: solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 320px;
+  padding: 30px;
+  margin: auto;
+  height: 50vh;
 }
 </style>
